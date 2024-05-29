bbb8e6a899d1a760a84310d9624c0e9b0438c386
Ayobami <68803580+ayobamiakomolafe@users.noreply.github.com>
2024-05-29 16:54:39 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,13 +10,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
 get_bearer_token = HTTPBearer(auto_error=False)
 known_tokens = set([os.getenv('BearerToken')])
 
 def transcribe_audio(file_path: str) -> str:
     model = whisper.load_model("tiny")
     text = model.transcribe(file_path, fp16=False)
-    os.remove(file_path)
     return text
     
 # Use Faster-whisper for much better speed compared to whisper tiny
@@ -38,29 +39,31 @@ app = FastAPI(
 class UnauthorizedMessage(BaseModel):
     detail: str = "Bearer token missing or unknown"
 
-@app.post("/api/v1/upload/", tags=["Speech2Text"])
-async def upload_file(file: UploadFile = File(description="mp3 file"), auth: Optional[HTTPAuthorizationCredentials] = Depends(get_bearer_token)):
+
+    
+@app.post("/audio", tags=["Speech2Text"])
+async def audio(file: UploadFile = File(description="mp3 file"), auth: Optional[HTTPAuthorizationCredentials] = Depends(get_bearer_token)):
     if auth is None or (token := auth.credentials) not in known_tokens:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=UnauthorizedMessage().detail,
         )
     
-    # Check file size
-    if file.content_type != "audio/mpeg" or len(await file.read()) >= 5242880:
+    
+
+    if "audio" in file.content_type or len(await file.read()) <= 5242880:
+        audio_source = "audio_source." + file.filename[-3:]
+        audio_content = await file.read()
+        open(audio_source, "wb").write(audio_content)
+        transcription = transcribe_audio(audio_source)
+        #With faster_whisper
+        #transcription = transcribe_audio_faster(audio_source)
+        os.remove(audio_source)
+        
+        return {"transcription": transcription}
+    else:
         raise HTTPException(status_code=400, detail="Invalid file format or size")
-
-    # Save the uploaded file
-    with open(file.filename, "wb") as f:
-        f.write(await file.read())
-
-    # Transcribe the audio file
-    transcription = transcribe_audio(file.filename)
-    # With faster_whisper
-    #transcription = transcribe_audio_faster(file.filename)
-
-    # Return the transcription result
-    return {"transcription": transcription}
+    
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=7070)
